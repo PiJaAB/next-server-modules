5c772e51825ac2bb3f140f9e4f59a1b3d1bcc6e3
Linn Dahlgren <linn.dahlgren@pija.se>
2020-03-02 16:35:04 +0100

    Actually added auth module to public exports, oops

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const base = require('./base');
 const sanityRedirect = require('./sanityRedirect');
 const defaultBabel = require('./defaultBabel');
 const applyMiddlewareIf = require('./applyMiddlewareIf');
+const createAuth = require('./auth');
 
 module.exports = {
   ...base,
   sanityRedirect,
   defaultBabel,
   applyMiddlewareIf,
+  createAuth,
 };
