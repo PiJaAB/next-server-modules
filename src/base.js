3191296536f0772689a06c3bf8d85ece5847e390
Linn Dahlgren <linn.dahlgren@pija.se>
2020-05-28 14:19:37 +0200

    Updated to new dotenv schema

--- src/base.js
+++ src/base.js
@@ -4,13 +4,32 @@
  * @typedef {import('express').NextFunction} NextFunction
  */
 
-const isProduction = process.env.NODE_ENV === 'production';
-const isStaging = process.env.NODE_ENV === 'staging';
-const isDev = !(isProduction || isStaging);
+if (process.env.NODE_ENV == null) process.env.NODE_ENV = 'development';
+if (
+  process.env.NODE_ENV !== 'development' ||
+  process.env.NODE_ENV !== 'production'
+) {
+  console.warn(`NODE_ENV set to unsupported value: ${process.env.NODE_ENV}`);
+}
 
-// Ensure that process.env.NODE_ENV is a known value.
-if (isDev) process.env.NODE_ENV = 'development';
-require('dotenv-load')();
+const isProduction = process.env.NODE_ENV === 'production';
+const isDev = process.env.NODE_ENV === 'development';
+
+const dotenvConfig = require('dotenv').config;
+
+function loadEnv(env = null) {
+  if (env == null) {
+    dotenvConfig({ path: '.env.override' });
+    dotenvConfig({ path: '.env' });
+  } else {
+    dotenvConfig({ path: `.env.${env}.override` });
+    dotenvConfig({ path: `.env.${env}` });
+  }
+}
+if (process.env.DEPLOY_ENV == null) process.env.DEPLOY_ENV = 'localdev';
+loadEnv(process.env.DEPLOY_ENV);
+loadEnv(process.env.NODE_ENV);
+loadEnv();
 
 const app = require('next')({ dev: isDev });
 
@@ -45,7 +64,6 @@ const start = init => {
 module.exports = {
   app,
   isDev,
-  isStaging,
   isProduction,
   handleNext,
   render404,
