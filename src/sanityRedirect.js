79685687fe526bff8688521374b805b6d0141f01
Linn Dahlgren <linn.dahlgren@pija.se>
2020-05-28 14:22:47 +0200

    fixed sanity redirect

--- src/sanityRedirect.js
+++ src/sanityRedirect.js
@@ -4,35 +4,42 @@
  * @typedef {import('express').NextFunction} NextFunction
  */
 
-const BASE_URL = process.env.NEXT_STATIC_BASE_URL;
-const PARSED_BASE = new URL(BASE_URL);
+try {
+  const BASE_URL = process.env.NEXT_STATIC_BASE_URL;
+  const PARSED_BASE = new URL(BASE_URL);
 
-/**
- * @param {Request} req
- * @param {Response} res
- * @param {NextFunction} next
- * @returns {void}
- */
-module.exports = function sanityRedirect(req, res, next) {
-  let sendRedirect = false;
-  let protocol = req.headers['x-forwarded-proto'];
-  let { url } = req;
+  /**
+   * @param {Request} req
+   * @param {Response} res
+   * @param {NextFunction} next
+   * @returns {void}
+   */
+  module.exports = function sanityRedirect(req, res, next) {
+    let sendRedirect = false;
+    let protocol = req.headers['x-forwarded-proto'];
+    let { url } = req;
 
-  if (protocol) {
-    protocol += ':';
-    if (protocol !== PARSED_BASE.protocol) sendRedirect = true;
-  }
-  if (req.headers.host !== PARSED_BASE.host) sendRedirect = true;
+    if (protocol) {
+      protocol += ':';
+      if (protocol !== PARSED_BASE.protocol) sendRedirect = true;
+    }
+    if (req.headers.host !== PARSED_BASE.host) sendRedirect = true;
 
-  const match = url.match(/(.+)\/($|\?.*)/);
+    const match = url.match(/(.+)\/($|\?.*)/);
 
-  if (match) {
-    url = `${match[1]}${match[2]}`;
-    sendRedirect = true;
-  }
+    if (match) {
+      url = `${match[1]}${match[2]}`;
+      sendRedirect = true;
+    }
 
-  if (sendRedirect) {
-    return res.redirect(301, `${BASE_URL}${url}`);
-  }
-  return next();
-};
+    if (sendRedirect) {
+      return res.redirect(301, `${BASE_URL}${url}`);
+    }
+    return next();
+  };
+} catch (err) {
+  module.exports = function sanityRedirect(req, res, next) {
+    console.error('Sanity redirect is not properly configured!');
+    return next();
+  };
+}
